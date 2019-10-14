861ed7caa8a4b165bc8fed0e46ab54a3abb12410
Erica <encannedy@gmail.com>
2019-10-14 00:09:57 -0400

    fixes to randomization and fixes to variable names. updated html to include button for images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,19 +2,26 @@ document.addEventListener("DOMContentLoaded",
     function () {
         var wins = 0;
         var loses = 0;
-        var cpuNumber = [Math.floor(Math.random() * (120 - 19) + 19)];
+        var userScore = 0;
+        var topaz = Math.floor(Math.random() * (12-1) +1);
+        var peridot = Math.floor(Math.random() * (12-1) +1);
+        var ruby = Math.floor(Math.random() * (12-1) +1);
+        var amethyst = Math.floor(Math.random() * (12-1) +1);
+
+        var cpuGuess = Math.floor(Math.random() * (120 - 19) + 19);
 
 
-        var winsText = document.getElementById("winsText");
-        var losesText = document.getElementById("losesText");
+        var winsText = $("#winsText");
+        var losesText = $("#losesText"); 
 
-        document.onkeyup = function (event) {
-            var userScore = event.key;
-            if (userScore === cpuNumber) {
+        $("#total-score").click = function (event) {
+            userScore = event.key;
+            if (userScore === cpuGuess) {
                 wins = wins + 1;
                 winsText.textContent = wins;
-                cpuNumber = [Math.floor(Math.random() * 2)];
+                cpuGuess = Math.floor(Math.random() * 2);
                 console.log(cpuGuess);
+                console.log (topaz);
             }
         }
     });
